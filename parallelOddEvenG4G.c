ff6aa35f193591bb628e31bcce4c1ce393cb6d39
dlcaio <caiodellalibera@id.uff.br>
2022-06-09 12:12:03 -0300

    Odd iteration apparently working

--- parallelOddEvenG4G.c
+++ parallelOddEvenG4G.c
@@ -17,11 +17,12 @@ void printArray(int *array, int n)
 
 void compareExchange(int *array, int pos, int odd)
 {
-  if (array[pos] > array[pos + 1])
+  int newPos = pos + odd;
+  if (array[newPos] > array[newPos + 1])
   {
-    int temp = array[pos];
-    array[pos] = array[pos + 1];
-    array[pos + 1] = temp;
+    int temp = array[newPos];
+    array[newPos] = array[newPos + 1];
+    array[newPos + 1] = temp;
   }
 }
 
@@ -38,59 +39,67 @@ int main(int argc, char **argv)
   for (int i = 0; i < ARRAY_SIZE; i++)
   {
     MPI_Barrier(MPI_COMM_WORLD);
+
     for (int j = 0; j < 2; j++)
     {
 
       int pos = (rank * 2) + j * 8;
       compareExchange(initial_array, pos, 0);
       MPI_Barrier(MPI_COMM_WORLD);
-      printf("process %d, i = %d, j = %d arr[%d, %d] is [%d, %d]\n", rank, i, j, pos, pos + 1, initial_array[pos], initial_array[pos + 1]);
-      
+      // printf("process %d, i = %d, j = %d arr[%d, %d] is [%d, %d]\n", rank, i, j, pos, pos + 1, initial_array[pos], initial_array[pos + 1]);
+
       MPI_Barrier(MPI_COMM_WORLD);
 
       MPI_Allgather(initial_array + pos, 2, MPI_INT, initial_array + j * 8, 2, MPI_INT, MPI_COMM_WORLD);
-      printArray(initial_array, 16);
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    for (int j = 0; j < 2; j++)
+
+    {
+
+      int pos = (rank * 2) + j * 8;
+
+      MPI_Comm ODD_COMM;
+
+      if (j == 1)
+      {
+        MPI_Comm_split(MPI_COMM_WORLD, rank < 3, rank, &ODD_COMM);
+      }
+      else
+      {
+        ODD_COMM = MPI_COMM_WORLD;
+      }
 
       MPI_Barrier(MPI_COMM_WORLD);
 
-      // printf("process %d, i = %d, j = %d arr[%d, %d] is [%d, %d]\n", rank, i, j, (rank * 2) + j * 8, (rank * 2) + j * 8 + 1, initial_array[(rank * 2) + j * 8], initial_array[(rank * 2) + j * 8 + 1]);
-      // printf("process %d, i = %d, j = %d arr[%d, %d] is [%d, %d]\n", rank, i, j, (rank * 2) + j * 8 + 1, (rank * 2) + j * 8 + 2, initial_array[(rank * 2) + j * 8  + 1], initial_array[(rank * 2) + j * 8 + 2]);
-      // Even iteration
-
-      // Compare exchange 0-1, 2-3...
-      // compareExchange(initial_array, (rank * 2) + j * 8, 0);
-
-      // Insert compared-exchanged values in initial array for all processes
-      // MPI_Allgather(initial_array + (rank * 2) + j * 8, 2, MPI_INT, initial_array, 2, MPI_INT, MPI_COMM_WORLD);
-
-      // Odd iteration
-
-      // Create communicator for odd iteration, since last process won't do work in this iteration
-      // MPI_Comm ODD_COMM;
-      // MPI_Comm_split(MPI_COMM_WORLD, (rank * 2) + j * 8 + 1 < 16 - 1, rank, &ODD_COMM);
-
-      // Check if process is the last one
-      /*
-      if ((rank * 2) + j * 8 + 1 != ARRAY_SIZE - 1)
+      if (pos + 1 != ARRAY_SIZE - 1)
       {
         // Compare exchange 1-2, 3-4...
-        compareExchange(initial_array, (rank * 2) + j * 8, 1);
+        compareExchange(initial_array, pos, 1);
 
         // Insert compared-exchanged values in initial array for all processes
-        MPI_Allgather(initial_array + (rank * 2) + j * 8 + 1, 2, MPI_INT, initial_array + 1, 2, MPI_INT, ODD_COMM);
+        MPI_Allgather(initial_array + pos + 1, 2, MPI_INT, initial_array + j * 8 + 1, 2, MPI_INT, ODD_COMM);
 
         // Send whole array from process 0 to last process
-        if (rank == 0)
+        if (rank == 0 && j == 1)
           MPI_Send(initial_array, ARRAY_SIZE, MPI_INT, size - 1, 0, MPI_COMM_WORLD);
       }
       // If process is the last one, receive whole array sent by process 0
       else
       {
+        
         MPI_Recv(initial_array, ARRAY_SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       }
-      */
-     MPI_Barrier(MPI_COMM_WORLD);
     }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    printArray(initial_array, 16);
+
     MPI_Barrier(MPI_COMM_WORLD);
   }
   MPI_Barrier(MPI_COMM_WORLD);

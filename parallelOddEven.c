31bd9ae78f3b3ace181c5869b6a5e9a8002cae47
Renan Valladão <renancvalladao@gmail.com>
2022-06-12 13:57:59 -0300

    Creating functions compareExchange right and left

--- parallelOddEven.c
+++ parallelOddEven.c
@@ -30,6 +30,22 @@ void printArray(int *array, int n) {
     printf("]\n");
 }
 
+void compareExchangeRight(int *innerArray, int *innerArrayToSort, int elementsPerArray, int rank) {
+    MPI_Send(innerArray, elementsPerArray, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+    MPI_Recv(innerArrayToSort + elementsPerArray, elementsPerArray, MPI_INT, rank + 1, 0, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+    bubbleSort(innerArrayToSort, elementsPerArray * 2);
+    memcpy(innerArray, innerArrayToSort, elementsPerArray * sizeof(int));
+}
+
+void compareExchangeLeft(int *innerArray, int *innerArrayToSort, int elementsPerArray, int rank) {
+    MPI_Recv(innerArrayToSort + elementsPerArray, elementsPerArray, MPI_INT, rank - 1, 0, MPI_COMM_WORLD,
+             MPI_STATUS_IGNORE);
+    MPI_Send(innerArray, elementsPerArray, MPI_INT, rank - 1, 0, MPI_COMM_WORLD);
+    bubbleSort(innerArrayToSort, elementsPerArray * 2);
+    memcpy(innerArray, innerArrayToSort + elementsPerArray, elementsPerArray * sizeof(int));
+}
+
 int main(int argc, char **argv) {
     int rank, size;
     int arraySize = atoi(argv[1]);
@@ -47,51 +63,37 @@ int main(int argc, char **argv) {
     MPI_Scatter(initialArray, elementsPerArray, MPI_INT, innerArray, elementsPerArray, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
     for (int i = 0; i < size; i++) {
-        int innerArray_to_sort[elementsPerArray * 2];
+        int innerArrayToSort[elementsPerArray * 2];
         MPI_Barrier(MPI_COMM_WORLD);
-        memcpy(innerArray_to_sort, innerArray, elementsPerArray * sizeof(int));
+        memcpy(innerArrayToSort, innerArray, elementsPerArray * sizeof(int));
         if (i % 2 == 0) { // Even iteration
             if (rank % 2 == 0) { // Even process number
                 if (rank < size - 1) {
                     // Compare-exchange with the right neighbor process
-                    MPI_Send(innerArray, elementsPerArray, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
-                    MPI_Recv(innerArray_to_sort + elementsPerArray, elementsPerArray, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                    bubbleSort(innerArray_to_sort, elementsPerArray * 2);
-                    memcpy(innerArray, innerArray_to_sort, elementsPerArray * sizeof(int));
+                    compareExchangeRight(innerArray, innerArrayToSort, elementsPerArray, rank);
                 }
             } else { // Odd process number
                 if (rank > 0) {
                     // Compare-exchange with the left neighbor process
-                    MPI_Recv(innerArray_to_sort + elementsPerArray, elementsPerArray, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                    MPI_Send(innerArray, elementsPerArray, MPI_INT, rank - 1, 0, MPI_COMM_WORLD);
-                    bubbleSort(innerArray_to_sort, elementsPerArray * 2);
-                    memcpy(innerArray, innerArray_to_sort + elementsPerArray, elementsPerArray * sizeof(int));
+                    compareExchangeLeft(innerArray, innerArrayToSort, elementsPerArray, rank);
                 }
             }
         } else { // Odd iteration
             if (rank % 2 == 1) { // Odd process number
                 if (rank < size - 1) {
                     // Compare-exchange with the right neighbor process
-                    MPI_Send(innerArray, elementsPerArray, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
-                    MPI_Recv(innerArray_to_sort + elementsPerArray, elementsPerArray, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                    bubbleSort(innerArray_to_sort, elementsPerArray * 2);
-                    memcpy(innerArray, innerArray_to_sort, elementsPerArray * sizeof(int));
+                    compareExchangeRight(innerArray, innerArrayToSort, elementsPerArray, rank);
                 }
             } else { // Even process number
                 if (rank > 0) {
                     // Compare-exchange with the left neighbor process
-                    MPI_Recv(innerArray_to_sort + elementsPerArray, elementsPerArray, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                    MPI_Send(innerArray, elementsPerArray, MPI_INT, rank - 1, 0, MPI_COMM_WORLD);
-                    bubbleSort(innerArray_to_sort, elementsPerArray * 2);
-                    memcpy(innerArray, innerArray_to_sort + elementsPerArray, elementsPerArray * sizeof(int));
+                    compareExchangeLeft(innerArray, innerArrayToSort, elementsPerArray, rank);
                 }
             }
         }
     }
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Gather(innerArray, elementsPerArray, MPI_INT, initialArray, elementsPerArray, MPI_INT, 0, MPI_COMM_WORLD);
-    printf("Inner Array process %d: ", rank);
-    printArray(innerArray, elementsPerArray);
     if (rank == 0) {
         printf("Sorted array: ");
         printArray(initialArray, arraySize);

0e2baf99069dc8d9edd5c007ecb056308879d0b2
dlcaio <caiodellalibera@id.uff.br>
2022-06-15 08:07:41 -0300

    Adding dynamic array for G4G version

--- parallelOddEvenG4G.c
+++ parallelOddEvenG4G.c
@@ -3,7 +3,13 @@
 #include <string.h>
 #include <mpi.h>
 
-#define ARRAY_SIZE 128
+void populateArray(int *array, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    array[i] = rand() % 100;
+  }
+}
 
 void printArray(int *array, int n)
 {
@@ -29,15 +35,19 @@ void compareExchange(int *array, int posWithoutOdd, int odd)
 int main(int argc, char **argv)
 {
   int rank, size;
-  int initial_array[ARRAY_SIZE] = {-61, -33, -83, -31, -1, 95, -72, -62, 94, 38, -70, 6, 74, -100, 61, 56, -5, -67, -56, 79, -50, 64, 9, 9, -85, 33, -54, 10, 66, 39, 18, 69, -78, -39, 15, -68, 64, -86, 63, 24, 26, -26, 47, 15, -35, -2, 37, 60, 23, -33, -76, -90, -17, 46, -46, 69, 54, 20, 72, -37, -44, -10, 8, 80, -74, 7, -12, -80, -78, -97, -87, 80, -70, 74, 45, 12, -8, 46, -35, 62, -69, 4, -50, -1, -100, -33, 0, -58, -30, -53, 85, -54, -2, 31, -62, -91, -40, 59, -24, -53, 33, 62, -72, 39, -72, 70, 92, -46, -72, 92, 46, -26, -2, 53, -33, 18, 56, 73, 59, -69, -76, 87, 32, -68, -28, -55, -55, -66};
+  int arraySize = atoi(argv[1]);
+  int initial_array[arraySize];
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  int nIterationsPerProcess = ARRAY_SIZE / (size * 2);
+  if (rank == 0) populateArray(initial_array, arraySize);
+  MPI_Bcast(initial_array, arraySize, MPI_INT, 0, MPI_COMM_WORLD);
 
-  // One even and one odd iteration for each i in ARRAY_SIZE
-  for (int i = 0; i < ARRAY_SIZE; i++)
+  int nIterationsPerProcess = arraySize / (size * 2);
+
+  // One even and one odd iteration for each i in arraySize
+  for (int i = 0; i < arraySize; i++)
   {
     for (int j = 0; j < nIterationsPerProcess; j++)
     {
@@ -52,7 +62,7 @@ int main(int argc, char **argv)
       else
         ODD_COMM = MPI_COMM_WORLD;
 
-      if (pos + 1 != ARRAY_SIZE - 1)
+      if (pos + 1 != arraySize - 1)
       {
         // Compare exchange 1-2, 3-4...
         compareExchange(initial_array, pos, 1);
@@ -62,16 +72,16 @@ int main(int argc, char **argv)
 
         // Send whole array from process 0 to last process
         if (rank == 0 && j == nIterationsPerProcess - 1)
-          MPI_Send(initial_array, ARRAY_SIZE, MPI_INT, size - 1, 0, MPI_COMM_WORLD);
+          MPI_Send(initial_array, arraySize, MPI_INT, size - 1, 0, MPI_COMM_WORLD);
       }
       // If process is the last one, receive whole array sent by process 0
       else
-        MPI_Recv(initial_array, ARRAY_SIZE, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(initial_array, arraySize, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
   }
 
   if (rank == 0)
-    printArray(initial_array, ARRAY_SIZE);
+    printArray(initial_array, arraySize);
 
   MPI_Finalize();
   return 0;

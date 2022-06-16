49c87969156d6aa8db6859b894ee8a6081381243
dlcaio <caiodellalibera@id.uff.br>
2022-06-16 13:06:27 -0300

    Working for odd iteration (arrSize = 8, size = 4)

--- MPI/parallelOddEvenG4G.c
+++ MPI/parallelOddEvenG4G.c
@@ -4,11 +4,31 @@
 #include <mpi.h>
 #include <time.h>
 
+void bubbleSort(int *array, int n) {
+    int temp;
+    for (int k = 1; k < n; k++) {
+        for (int j = 0; j < n - k; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
 void populateArray(int *array, int n) {
     srand(time(NULL));
     for (int i = 0; i < n; i++) {
         array[i] = rand() % 100;
     }
+}
+
+int isCorrect(int *array, int* sane, int nSane) {
+  for (int i = 0; i < nSane; i++) {
+    if (array[i] != sane[i]) return 0;
+  }
+  return 1;
 }
 
 int isSorted(int *array, int n) {
@@ -43,6 +63,7 @@ int main(int argc, char **argv) {
     int rank, size;
     int arraySize = atoi(argv[1]);
     int initialArray[arraySize];
+    int saneArray[arraySize];
     double duration = 0.0;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -51,8 +72,10 @@ int main(int argc, char **argv) {
     if (rank == 0) {
         printf("Parallel OddEven GeeksForGeeks Version\n");
         populateArray(initialArray, arraySize);
-//        printf("Array to be sorted: ");
-//        printArray(initialArray, arraySize);
+        populateArray(saneArray, arraySize);
+        bubbleSort(saneArray, arraySize);
+        printf("Array to be sorted: ");
+        printArray(initialArray, arraySize);
         duration -= MPI_Wtime();
     }
     MPI_Bcast(initialArray, arraySize, MPI_INT, 0, MPI_COMM_WORLD);
@@ -81,14 +104,26 @@ int main(int argc, char **argv) {
         MPI_Allgather(initialArray + rank * 2 * nIterationsPerProcess, 2 * nIterationsPerProcess, MPI_INT, initialArray,  2 * nIterationsPerProcess, MPI_INT, MPI_COMM_WORLD);
 
         for (int j = 0; j < nIterationsPerProcess; j++) {
-            int pos = (rank * 2) + j * 2 * size;
+            int pos = (rank * 2) * nIterationsPerProcess + 2 * j;
 
-            MPI_Comm ODD_COMM;
-            if (j == nIterationsPerProcess - 1)
-                MPI_Comm_split(MPI_COMM_WORLD, rank < size - 1, rank, &ODD_COMM);
-            else
-                ODD_COMM = MPI_COMM_WORLD;
+            if (pos + 1 != arraySize - 1) {
+              compareExchange(initialArray, pos, 1);
+            } 
+          }
+          int s = 2;
+          if (rank == size - 1) s = 1;
 
+          int counts[4] = {2, 2, 2, 1};
+          int displacements[4] = {1, 3, 5, 7};
+
+          MPI_Allgatherv(initialArray + rank * 2 * nIterationsPerProcess + 1, s, MPI_INT, initialArray, counts, displacements, MPI_INT, MPI_COMM_WORLD);
+
+          //p0 = 1
+          //p1 = 3
+          //p2 = 5
+          //p3 = 7
+
+            /*
             if (pos + 1 != arraySize - 1) {
                 // Compare exchange 1-2, 3-4...
                 compareExchange(initialArray, pos, 1);
@@ -104,15 +139,17 @@ int main(int argc, char **argv) {
                 // If process is the last one, receive whole array sent by process 0
             else
                 MPI_Recv(initialArray, arraySize, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
+            }
+            */
+        
     }
 
     if (rank == 0) {
         duration += MPI_Wtime();
-//        printf("Sorted array: ");
-//        printArray(initialArray, arraySize);
+        printf("Sorted array: ");
+        printArray(initialArray, arraySize);
         printf("Duration: %lfs\n", duration);
-        printf("Is sorted? %d\n", isSorted(initialArray, arraySize));
+        printf("Is correct? %d\n", isCorrect(initialArray, saneArray, arraySize));
     }
 
     MPI_Finalize();
